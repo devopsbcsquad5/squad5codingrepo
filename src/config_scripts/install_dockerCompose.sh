104701361a047ed091ea36f82e457bc414ae30d7
ManojKumar Selvaraju <smanoj85@gmail.com>
2021-03-01 18:25:20 -0500

    Updated the install_dockerCompose.sh file as Linux-x86_64

--- src/config_scripts/install_dockerCompose.sh
+++ src/config_scripts/install_dockerCompose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo curl -L "https://github.com/docker/compose/releases/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose ;
+sudo curl -L "https://github.com/docker/compose/releases/1.28.2/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose ;
 sudo chmod +x /usr/local/bin/docker-compose; 
 docker-compose --version; 
 sudo sysctl -w vm.max_map_count=262144
