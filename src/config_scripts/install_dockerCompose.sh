ad8cd9d4f0adcd834a5785529c227c2e9c9afa19
ManojKumar Selvaraju <smanoj85@gmail.com>
2021-03-01 18:57:53 -0500

    Corrected the docker-compose download URL

--- src/config_scripts/install_dockerCompose.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose ;
-sudo chmod +x /usr/local/bin/docker-compose; 
-docker-compose --version; 
-sudo sysctl -w vm.max_map_count=262144
-docker-compose up -d; 
-docker ps
